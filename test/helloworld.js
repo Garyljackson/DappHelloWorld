c36074f5a8ddf24e3c12e87ac75222ce3654cd0b
Gary Jackson <garyljackson@gmail.com>
2021-10-13 14:28:09 +1000

    Add contract migration

---
+++ test/helloworld.js
@@ -0,0 +1,28 @@
+const HelloWorld = artifacts.require("HelloWorld");
+
+contract("HelloWorld", (accounts) => {
+  it("should initialise the greeting", async () => {
+    const HelloWorldInstance = await HelloWorld.deployed();
+    const greeting = await HelloWorldInstance.greeting.call();
+
+    assert.equal(
+      greeting,
+      "Initial Greeting",
+      "The initial greeting was not set"
+    );
+  });
+
+  it("should update the greeting", async () => {
+    const HelloWorldInstance = await HelloWorld.deployed();
+
+    await HelloWorldInstance.setGreeting("Howzit!");
+
+    const greeting = await HelloWorldInstance.greeting.call();
+
+    assert.equal(
+      greeting,
+      "Howzit!",
+      "The greeting was not updated"
+    );
+  });
+});
