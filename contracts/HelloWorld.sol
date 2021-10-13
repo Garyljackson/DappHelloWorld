fcc927f282a9d5d262857375b6238f516a6bdbc2
Gary Jackson <garyljackson@gmail.com>
2021-10-13 14:28:47 +1000

    Implement HelloWorld contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract HelloWorld {}
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Initial Greeting";
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+}
